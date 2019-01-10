477455da896790d3dd8604927477a9b16a2a292b
dsdunn <dsdunnmusic@gmail.com>
2019-01-10 13:46:45 -0700

    Move trigger down

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-const exitObj = {opacity: 0, paused: true}
-const entryFromObj = {y: 600, opacity: 0, paused: true};
-const entryToObj = {y: -200, opacity: 1, paused: true};
+const exitObj = {opacity: 0, scale: 0.9, paused: true}
+const entryFromObj = {y: 600, opacity: 0, scale: 0.7, paused: true};
+const entryToObj = {y: -200, opacity: 1, scale: 1, paused: true};
 
 const titleExit = TweenMax.to('#title', 3, exitObj);
 const projectsEntry = TweenMax.fromTo('#projects', 6, entryFromObj, entryToObj);
@@ -8,7 +8,7 @@ const projectsEntry = TweenMax.fromTo('#projects', 6, entryFromObj, entryToObj);
 const projectsExit = TweenMax.to('#projects', 3, exitObj);
 const experienceEntry = TweenMax.fromTo('#experience', 6, entryFromObj, entryToObj);
 
-const experienceExit = TweenMax.to('#experience', 3, {opacity: 0, paused: true});
+const experienceExit = TweenMax.to('#experience', 3, exitObj);
 const aboutEntry = TweenMax.fromTo('#about', 6, entryFromObj, entryToObj);
 
 const aboutExit = TweenMax.to('#about', 3, exitObj);
@@ -23,14 +23,14 @@ $(window).scroll(() => {
     let depth = triggerDepth($(trigger));
 
 
-    if (depth < 550) {
+    if (depth < 650) {
       move(trigger, depth);
     }
   }
 })
 
 const move = (trigger, depth) => {
-  let fraction = Math.pow(depth - 550, 2) / Math.pow(200, 2);
+  let fraction = Math.pow(depth - 650, 2) / Math.pow(200, 2);
 
   switch (trigger) {
     case '#trigger-top':
