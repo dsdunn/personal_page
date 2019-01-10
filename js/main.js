24958b8de5bf218b8648fd0ef357b97ce8007f0f
dsdunn <dsdunnmusic@gmail.com>
2019-01-09 18:27:49 -0700

    Begin project section animation

--- js/main.js
+++ js/main.js
@@ -1,6 +1,29 @@
+let windowHeight = window.innerHeight;
 
-let stackToggle = $('.stack-toggle');
+let titleExit = TweenMax.to('#title', 2, {y: -900, ease: Power4.easeOut, paused: true});
 
-stackToggle.click(() => {
-  $('.stack-collapse').toggleClass('stack-expanded');
+let projectsEntry = TweenMax.from('#projects', 3, {y: 275, opacity: 0, paused: true});
+
+$(window).resize(() => {
+  windowHeight = window.innerHeight;
+});
+
+$(window).scroll(() => {
+
+  if (elTopPos($('#projects')) > 0) {
+    let fraction = $(window).scrollTop()  / $(this).height();
+    console.log(fraction)
+    projectsEntry.progress(fraction);
+   }
+
 })
+
+const elTopPos = (element) => window.innerHeight + window.scrollY - element.offset().top;
+
+const elBottomPos = (element) => {
+  return window.scrollY - element.offset().top + element.height() ;
+}
+
+const progress = (element) => {
+
+}
